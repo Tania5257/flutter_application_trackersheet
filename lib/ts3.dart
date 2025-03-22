4e16a2d49c724eb64fa5af866fd91860440aaf51
Tania5257 <tania15-5257@diu.edu.bd>
2025-03-22 20:06:18 +0600

    trackersheet3

---
+++ lib/ts3.dart
@@ -0,0 +1,44 @@
+//3. Create an app that displays a list of items using a ListView. Each item should be a text widget with a unique number.
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ListView Example Tracker Sheet 3'),
+          backgroundColor: Colors.blue, // Greenish header
+        ),
+        body: ItemList(),
+      ),
+    );
+  }
+}
+
+class ItemList extends StatelessWidget {
+  final List<String> items = [
+    'Mobile Application Design', 'Computer Architecture & Organization', 'Compiler Design', 'Artificial Intelligence',
+    'Mobile Application Design Lab', 'Compiler Design Lab', 'Artificial Intelligence Lab'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: items.length,
+      separatorBuilder: (context, index) => Divider(), // Adds line separator
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text('${index + 1}. ${items[index]}'),
+        );
+      },
+    );
+  }
+}
